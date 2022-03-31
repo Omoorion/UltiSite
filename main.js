31e600167b581fb0554ee647dfdb3357947ff9dd
Omoorion <86531601+Omoorion@users.noreply.github.com>
2022-03-31 14:22:05 +0200

    added code

---
+++ main.js
@@ -0,0 +1,74 @@
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js'
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js'
+import Stats from 'https://unpkg.com/three@0.126.1/examples/jsm/libs/stats.module.js'
+import {GUI} from 'https://unpkg.com/three@0.126.1/examples/jsm/libs/dat.gui.module'
+import * as TWEENMAX from 'https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const canvas = document.getElementById("canvas_container")
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true,
+})
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+document.body.appendChild( renderer.domElement );
+//new OrbitControls(camera, renderer.domElement) //ROTATES CAMERA, NOT OBJECT, NOT LIGHT
+//THEREFORE MUST LIGHT THE BACK OF THE OBJECT TOO IF YOU WANT IT VISIBLE
+
+const loader = new THREE.TextureLoader();
+const geometry = new THREE.SphereGeometry(1, 64, 32);
+const material = new THREE.MeshBasicMaterial( {map: loader.load('/RickFace.jpg'), } );
+const sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+
+camera.position.z = 5;
+var position = window.scrollY;
+
+window.addEventListener("resize", () => {
+    // Update sizes
+
+    // Update camera
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  })
+
+function createVector(x, y, z, camera, width, height) {
+    var p = new THREE.Vector3(x, y, z);
+    var vector = p.project(camera);
+
+    vector.x = (vector.x + 1) / 2 * width;
+    vector.y = -(vector.y - 1) / 2 * height;
+
+    return vector;
+}
+
+// function updateSphere(){
+//     var scroll = window.scrollY;
+//     var ypos = createVector(sphere.position.x, sphere.position.y, sphere.position.z, camera, window.innerWidth, window.innerHeight).y
+//     console.log(ypos)
+//     if (scroll > position) {
+//         sphere.position.y -= 0.175;
+//     } 
+//     else if (scroll < position) {
+//         sphere.position.y += 0.15;
+//     }
+//     position = scroll;
+// }
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    sphere.rotation.y += 0.01;
+    //updateSphere()
+    renderer.render( scene, camera );
+};
+
+animate();
---
+++ videoplay.js
@@ -0,0 +1,110 @@
+//Vars
+var video = document.getElementById("video")
+var animtxt = document.getElementById("desc")
+var maintxt = document.getElementById("maindesc")
+var bait = document.getElementById("bait")
+var pbtn = document.getElementById("pbtn")
+var txtbait = document.getElementById("txtbait")
+var body = document.getElementById("body")
+var maintaince = document.getElementById("maintaince")
+var spans = animtxt.children
+let first = true
+
+//Setup
+maintaince.style.visibility="hidden"
+pbtn.style.visibility="hidden"
+video.pause()
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0)
+}
+//Events
+video.addEventListener("play", StartAnim)
+video.addEventListener("pause", stopAnim)
+video.addEventListener("ended", stopAnim)
+bait.addEventListener("click", ()=>{
+    if(first){
+        bait.remove()
+        txtbait.remove()
+        body.style.background = "#000"
+        scrollToElement(maintxt)
+        video.play()
+        video.volume=0.6
+        video.muted = !video.muted
+        animtxt.style.visibility="hidden" //for now..
+        //animtxt.style.opacity=1
+        //MAKE BACKGROUND REALLY DARK WHEN YOU PRESS AND THEN LIGHT IT UP AFTER 2 SECONDS WITH THE REST OF THE STUFF 
+        sleep(2000).then(()=>{
+            body.style.background = "rgb(25, 0, 37)"
+            maintxt.style.opacity=1
+            video.style.opacity=1
+            pbtn.style.visibility="visible"
+            pbtn.style.opacity=1
+            //LIGHT IT UP HERE OR MAYBE IN STARTANIMATION()
+         })
+        first=false
+    }
+})
+
+pbtn.addEventListener("click", ()=>{
+    var vol = video.volume
+    var interval = 150 // 150ms interval
+
+    var fadeout = setInterval(
+        function() {
+            // Reduce volume by 0.05 as long as it is above 0
+            // This works as long as you start with a multiple of 0.05!
+            if (vol > 0) {
+            vol -= 0.05
+            video.volume = vol
+            }
+            else {
+            // Stop the setInterval when 0 is reached
+            clearInterval(fadeout)
+            }
+        }, interval)
+    video.style.opacity=0
+    pbtn.style.opacity=0
+    maintxt.style.opacity=0
+    sleep(2000).then(()=>{
+        video.style.visibility="hidden"
+        pbtn.style.visibility="hidden"
+        maintaince.style.visibility="visible"
+    })
+    //then start game in here.
+})
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function StartAnim(){
+    for (let i=0; i<spans.length; i++){
+        spans[i].style.animationPlayState = 'running'
+    }
+    maintxt.style.animationPlayState = 'running'
+    video.style.animationPlayState='running'
+    pbtn.style.animationPlayState='running'
+    //handle the nevergonna glowing delay in the middle of the choir
+}
+
+function stopAnim(){
+    for (let i=0; i<spans.length; i++){
+        spans[i].style.animationPlayState = 'paused'
+    }
+    maintxt.style.animationPlayState = 'paused'
+    video.style.animationPlayState='paused'
+    pbtn.style.animationPlayState='paused'
+}
+
+function scrollToElement(pageElement) {    
+    var positionX = 0,         
+        positionY = 0
+
+    while(pageElement != null){        
+        positionX += pageElement.offsetLeft       
+        positionY += pageElement.offsetTop       
+        pageElement = pageElement.offsetParent       
+        window.scrollTo(positionX, positionY)   
+    }
+}
